95bc1a58dd0fa257f1f27adb773b580f30a4057b
hachimei <940743130@qq.com>
2019-09-04 23:25:22 +0800

    init demo

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: '2018',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.test.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <formDemo />
+  </div>
+</template>
+
+<script>
+import formDemo from './components/formDemo'
+
+export default {
+  name: 'app',
+  components: {
+    formDemo,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
